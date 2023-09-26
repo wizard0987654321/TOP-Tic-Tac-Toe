fa6a7e75127f61f2ef1d0884f96e695d12a39240
givisab <givisab12345@gmail.com>
2023-09-26 13:34:50 +0400

    Finish JS, add win logic

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const board = document.querySelector(".gameboard");
+let winText = document.querySelector("h2");
 let xTurn = true;
 
 // Create gameboard grid
@@ -17,47 +18,95 @@ const Gameboard = (() => {
 
     const change = (choiceIndex, xTurn) => {
         if (xTurn) {
-            xChoices.push(choiceIndex);
-        } else {
             oChoices.push(choiceIndex);
+        } else {
+            xChoices.push(choiceIndex);
         }
     }
 
-    return { change, xChoices, oChoices };
-})();
-
-const Player = (choice) => {
-
-    const getChoice = () => {
-        return choice;
+    const reset = () => {
+        xChoices.length = 0;
+        oChoices.length = 0;
     }
 
-    return {getChoice}
-}
+    return { change, xChoices, oChoices, reset };
+})();
+
 
 const game = (() => { 
 
 
-const boardCells = Array.from(document.querySelectorAll(".square"));
+let isOver = false;
 
-    for (let i = 0; i < 9; i++) { 
-        boardCells[i].setAttribute("data-cell-index", i); 
-        boardCells[i].addEventListener("click", (e) => {
-            if(e.target.textContent == "") {
-                if(xTurn) {
-                    e.target.textContent = "X";
-                    xTurn = false;
-                } else {
-                    e.target.textContent = "O"
-                    xTurn = true;
-                }
-                let turnIndex = e.target.getAttribute("data-cell-index");
-                Gameboard.change(turnIndex, xTurn);
-                console.log(Gameboard.xChoices);
-                console.log(Gameboard.oChoices);
-            }
-        })
+const checkCombo = (combo, round) => {
+    const winCombos = [
+        [0, 1, 2],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    const isWon = winCombos.some(winCombo => {
+        return winCombo.every(index => combo.includes(index));
+    });
+
+    isOver = (round == 9) ? true : false;
+
+    if(!isWon && isOver) {
+        winText.textContent = "It's a draw!";
+        isOver = true;
     }
+
+    if(isWon && (round % 2 != 0)) {
+        winText.textContent = "Congratulations X, You won!"
+        isOver = true;
+    } else if (isWon && (round % 2 == 0)) {
+        winText.textContent = "Congratulations O, You won!"
+        isOver = true;
+    }
+}
+
+const boardCells = Array.from(document.querySelectorAll(".square"));
+let roundCount = 0;
+
+for (let i = 0; i < 9; i++) { 
+    boardCells[i].setAttribute("data-cell-index", i); 
+    boardCells[i].addEventListener("click", (e) => {
+        if(e.target.textContent == "" && !isOver) {
+            if(xTurn) {
+                e.target.textContent = "X";
+                xTurn = false;
+            } else {
+                e.target.textContent = "O"
+                xTurn = true;
+            }
+            let turnIndex = parseInt(e.target.getAttribute("data-cell-index"));
+            roundCount++;
+            Gameboard.change(turnIndex, xTurn);
+            if(roundCount % 2 != 0) {
+                checkCombo(Gameboard.xChoices, roundCount)
+            } else {
+                checkCombo(Gameboard.oChoices, roundCount);
+            }
+        }
+    })
+}
+
+const resetButton = document.querySelector("button");
+
+resetButton.addEventListener("click", () => {
+    boardCells.forEach(cell => cell.textContent = "");
+    roundCount = 0;
+    isOver = false;
+    isWon = false;
+    xTurn = true;
+    Gameboard.reset();
+    winText.textContent = "Player X Turn"
+})
     
 })();
 
